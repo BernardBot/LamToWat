ec5bdcb4904d1bbc422d6a480602112399010867
ben <ben@bens-MacBook-Pro.local>
2021-04-25 08:01:35 +0200

    Better run

--- src/Run.hs
+++ src/Run.hs
@@ -11,11 +11,8 @@ import Development.Shake
 import Val
 import Commands
 import Union
-import Interpreter
 import Types hiding (Fix)
 import qualified Types as T (Fix)
-
-import Lam.Parser
 
 import Lam.Syntax
 import Cps.Syntax
@@ -29,7 +26,7 @@ import Trans.Lam2Cps
 import Trans.Cps2Hps
 import Trans.Hps2Wat
 
-import Trans.Lam2Tree hiding (LamTree)
+import Trans.Lam2Tree
 import Trans.Tree2Tps
 import Trans.Tps2Tps
 import Trans.Tps2Wat
@@ -41,22 +38,16 @@ runWatFile watfile wasmfile = do
   where wat2wasm = "/Users/ben/wabt/bin/wat2wasm"
         wasminterp = "/Users/ben/wabt/bin/wasm-interp"
 
-runCps :: String -> EDom
-runCps =
-  run0 .
+transCps =
   (transform :: Hps -> Wat) .
   (transform :: Cps -> Hps) .
-  (transform :: Lam -> Cps) .
-  parseLam'
+  (transform :: Lam -> Cps)
 
-runTps :: String -> EDom
-runTps =
-  run0 .
+transTps =
   (transform :: T.Fix (Tps (Malloc :+: Base :+: Empty) Val) -> Wat) .
-  (transform :: Tps (Fix :+: Malloc :+: Base :+: Empty) Val -> T.Fix (Tps (Malloc :+: Base :+: Empty) Val)) .
+  (transform :: Tps (Fix :+: Malloc :+: Base :+: Empty) Val -> T.Fix (Tps (Malloc :+: Base :+: Empty)  Val)) .
   (transform :: Tps (Malloc :+: Fix :+: Base :+: Empty) Val -> Tps (Fix :+: Malloc :+: Base :+: Empty) Val) .
   (transform :: Tps (Record :+: Fix :+: Base :+: Empty) Val -> Tps (Malloc :+: Fix :+: Base :+: Empty) Val) .
-  (transform :: Tps (Fix :+: Base :+: Empty)            Val -> Tps (Record :+: Fix :+: Base :+: Empty) Val) .
-  (transform :: Tree (Comp :+: Fix :+: Base)            Val -> Tps (Fix :+: Base :+: Empty) Val) .
-  (transform :: Lam                                         -> Tree (Comp :+: Fix :+: Base) Val) .
-  parseLam'
+  (transform :: Tps            (Fix :+: Base :+: Empty) Val -> Tps (Record :+: Fix :+: Base :+: Empty) Val) .
+  (transform :: Tree  (Comp :+: Fix :+: Base)           Val -> Tps            (Fix :+: Base :+: Empty) Val) .
+  (transform :: Lam                                         -> Tree  (Comp :+: Fix :+: Base)           Val)
