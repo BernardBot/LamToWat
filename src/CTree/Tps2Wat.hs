b6894b5cada257938b3263f124d1115e60e05454
ben <ben@bens-MacBook-Pro.local>
2021-04-27 14:17:09 +0200

    tps2wat within margin

--- src/CTree/Tps2Wat.hs
+++ src/CTree/Tps2Wat.hs
@@ -25,17 +25,18 @@ type WatTps = Fix (Tps (Malloc :+: Base :+: Empty) Val)
 
 tps2wat :: WatTps -> Wat
 tps2wat (fs,e) = (map (fmap t2w) fs,t2w e)
-    where ns = map (\ (f,as,b) -> f) fs
+  where ns = map (\ (f,as,b) -> f) fs
 
-          t2w (Leaf v)                                       = Done (v2v v)
-          t2w (Node    (L (T.Malloc i))    Nil (Some (x,k))) = Malloc i x (t2w k)
-          t2w (Node    (L (T.Load i v))    Nil (Some (x,k))) = Load i (v2v v) x (t2w k)
-          t2w (Node    (L (T.Store i s t)) Nil (Some (_,k))) = Store i (v2v s) (v2v t) (t2w k)
-          t2w (Node (R (L (T.Add v1 v2)))  Nil (Some (x,k))) = Add (v2v v1) (v2v v2) x (t2w k)
-          t2w (Node (R (L (T.App v vs)))   Nil None)         = App (v2v v) (map v2v vs)
+        t2w (Leaf v) = Done (v2v v)
+        t2w (Node cmd ks k) = case (cmd,ks,k) of
+          ((L (T.Malloc i)),      Nil, (Some (x,k))) -> Malloc i x (t2w k)
+          ((L (T.Load i v)),      Nil, (Some (x,k))) -> Load i (v2v v) x (t2w k)
+          ((L (T.Store i s t)),   Nil, (Some (_,k))) -> Store i (v2v s) (v2v t) (t2w k)
+          ((R (L (T.Add v1 v2))), Nil, (Some (x,k))) -> Add (v2v v1) (v2v v2) x (t2w k)
+          ((R (L (T.App v vs))),  Nil, None)         -> App (v2v v) (map v2v vs)
 
-          v2v (LABEL x) = INT $ fromJust $ x `elemIndex` ns
-          v2v v         = v
+        v2v (LABEL x) = INT $ fromJust $ x `elemIndex` ns
+        v2v v         = v
 
 instance Emitable WatTps where
   emit = emit . tps2wat
